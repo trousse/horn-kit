cabd8f38af1f18ff531f17fecb2f77f009706e0f
thomas <ths.rousse@gmail.com>
2021-01-11 23:21:07 +0100

    categorie minimal

--- src/App.js
+++ src/App.js
@@ -47,12 +47,83 @@ const items = [{ images : categorie_caisson_de_basse , videoSRC: 'https://www.yo
               { images : hautParleur , videoSRC: 'https://www.youtube.com/watch?v=-2opH8aG9Ag', code: '004' , pdfSRC: './resultat.pdf'},]
 
 const categories = [
-{src: caisson2 , name: 'Sonorisation de puissance', id: 1},
-{src: categorie_caisson_de_basse , name: 'Sonorisation de salon', id: 2},
-{src: lampe, name: 'Mise en scene', id: 3},
-{src: hautParleur, name: 'Haut parleurs', id: 4},
-{src: acessoire, name: 'Accessoires', id: 5},
-{src: surMesure, name: 'Sur mesures', id: 6},
+  {src: caisson2 , name: 'Sonorisation de puissance', subCategories:[
+      {src: caisson2 , name: 'Sets complets',link:''},
+      {src: categorie_caisson_de_basse , name: 'Aigus - piézo',link:''},
+      {src: caisson2 , name: 'Mediums - Bass', link:''},
+      {src: surMesure , name: 'Pièces détachées', subCategories:[
+          {src: caisson2 , name: 'Grilles de protection',link:''},
+          {src: caisson2 , name: 'Haut-Parleurs',link:''},
+          {src: caisson2 , name: 'Platines pour connectiques',link:''},
+          {src: caisson2 , name: 'Coins & Frettes',link:''},
+          {src: caisson2 , name: 'Isolation Phonique',link:''},
+          {src: caisson2 , name: 'Peintures',link:''},
+        ]},
+    ]},
+  {src: categorie_caisson_de_basse , name: 'Sonorisation de salon', subCategories:[
+    {src: caisson2 , name: 'Enceintes Passives'},
+    {src: caisson2 , name: 'Enceintes Actives'},
+    {src: caisson2 , name: 'Enceintes Sans Fil'},
+    {src: caisson2 , name: 'Pièces Détacées',subCategories:[
+        {src: caisson2 , name: 'Amplificateur encastrable'},
+        {src: caisson2 , name: 'completer'},
+        {src: caisson2 , name: 'completer'},
+        {src: caisson2 , name: 'completer'},
+        {src: caisson2 , name: 'completer'},
+        {src: caisson2 , name: 'completer'},
+      ]},
+   ]},
+  {src: lampe, name: 'Enceintes portables',subCategories:[
+      {src: caisson2 , name: 'Enceintes Vintage'},
+      {src: caisson2 , name: 'Enceintes Sac-à-Dos'},
+      {src: caisson2 , name: 'Autocollants'},
+      {src: caisson2 , name: 'pièces Détachées',subCategories:[
+          {src: caisson2 , name: 'completer'},
+          {src: caisson2 , name: 'completer'},
+      ]},
+   ]},
+  {src: hautParleur, name: 'Haut parleurs',subCategories:[
+    {src: caisson2 , name: 'Projecteurs Laser'},
+    {src: caisson2 , name: 'Pyrotechnie'},
+    {src: caisson2 , name: 'Machines à fumer',subCategories:[
+        {src: caisson2 , name: 'Electrique'},
+        {src: caisson2 , name: 'Gaz'},
+        {src: caisson2 , name: 'pièces détachées'},
+    ]},
+    {src: caisson2 , name: 'Pilotage MIDI',subCategories:[
+      {src: caisson2 , name: 'Contrôleurs MIDI'},
+      {src: caisson2 , name: 'Câbles MIDI'},
+      {src: caisson2 , name: 'Récepteurs MIDI'},
+     ]},
+   ]},
+  {src: acessoire, name: 'Câbles & connectiques',subCategories:[
+      {src: caisson2 , name: 'Cables et Embases MIDI'},
+      {src: caisson2 , name: 'Enceintes Actives'},
+   ]},
+  {src: surMesure, name: 'Pièces Détachées',subCategories:[
+      {src: caisson2 , name: 'Sonorisation de puissance', subCategories:[
+          {src: caisson2 , name: 'Grilles de protection'},
+          {src: caisson2 , name: 'Haut-Parleurs',link:''},
+          {src: caisson2 , name: 'Platines pour connectiques'},
+          {src: caisson2 , name: 'Coins & Frettes'},
+          {src: caisson2 , name: 'Isolation Phonique'},
+          {src: caisson2 , name: 'Peintures'},
+        ]},
+      {src: caisson2 , name: 'sonorisation HIFI',subCategories:[
+          {src: caisson2 , name: 'Amplificateur encastrable'},
+          {src: caisson2 , name: 'completer'},
+          {src: caisson2 , name: 'completer'},
+          {src: caisson2 , name: 'completer'},
+          {src: caisson2 , name: 'completer'},
+          {src: caisson2 , name: 'completer'},
+        ]},
+      {src: caisson2 , name: 'Encaintes Portables',subCategories:[
+          {src: caisson2 , name: 'completer'},
+          {src: caisson2 , name: 'completer'},
+      ]},
+      {src: caisson2 , name: 'Projecteurs Laser'},
+      {src: caisson2 , name: 'Machines à Fumer'},
+  ]},
 ]
 
 const black = grey[900];
@@ -102,14 +173,14 @@ class App extends Component {
             <ToolBar position = "sticky"/>
             <Presentation images = {images} interval = {4000}/>
             <Space size={6}/>
-            <Categories categories = {categories}/>
+            <Categories tree = {categories}/>
             <Space size={6}/>
             <Contents docSearcherProps = {{item: item , onClick: (code)=>{ this.handleCodeSubmit(code) }}}/>
             <Typography component="div" style={{ backgroundColor: theme.palette.primary.light, height: '50vh' }}/>
           </Container>
         </ThemeProvider>
       </div>
-    );
+    )
   }
 
 
--- src/catégorie.js
+++ src/catégorie.js
@@ -1,65 +1,129 @@
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
+import { Component } from 'react';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import IconButton from '@material-ui/core/IconButton';
 
 
-const useStyles =  makeStyles((theme) => ({
-    root:{
-      flexGrow : 1,
-      height : '90vh'
-    },
-    img:{
-      maxWidth: 100+"%",
-      maxHeight: 100+"%"
-    },
-    categorie:{
-        maxWidth: 33+"%",
+const styles = (theme) => ({
+      root:{
+        flexGrow : 1,
+        backgroundColor:'white',
+        position: 'relative',
+      },
+      img:{
+        maxWidth: 100+"%",
+        maxHeight: 100+"%",
         cursor: "pointer"
-    },
-    categories:{
-      backgroundColor: 'white'
-    },
-    title: {
-      fontFamily:  "Jazz LET, fantasy",
-      fontSize: 35
+      },
+      categorie:{
+        marginLeft: '2%',
+        marginRight:'2%'
+      },
+      categories:{
+        backgroundColor: 'white',
+      },
+      title: {
+        fontFamily:  "Jazz LET, fantasy",
+        fontSize: 35,
+        cursor:'default',
+        position: 'relative',
+      },
+      arrow:{
+        position : 'absolute',
+        top:'0px',
+        left:'10%',
+      },
+      titleContainer:{
+        position: 'relative'
+      },
+      name: {
+        fontSize: 20,
+        cursor:'default',
+      }
+  });
+
+class Categories extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      classes: props.classes,
+      curent: props.tree,
+      path:[],
+      title:""
+    };
+  }
+
+  handleClick(index){
+    const {curent} = this.state
+    const {name,subCategories} = curent[index]
+    let {path} = this.state
+
+    if(subCategories){
+      path.push(curent)
+      this.setState({curent:subCategories,path:path,title:name})
     }
-}));
+    else{
+      console.log('go to ' + name)
+    }
+  }
 
-export default function Categories({categories=[]}){
-  const classes = useStyles();
-  return(
+  getBack(){
+    let {path} = this.state
+    const last = path.pop()
+    this.setState({curent : last , path : path})
+  }
 
-    <div className = {classes.root}>
-      <Typography component="div" style={{ backgroundColor: 'white', height: '10vh' }}>
-        <Typography className={classes.title}>
-            CATEGORIES
+  render(){
+    const {classes,curent,path,title} = this.state
+    return(
+      <div className = {classes.root}>
+        <Typography component="div" style={{ backgroundColor: 'white', height: '10%' }}>
+          {path[0] ?(
+            <div className = {classes.titleContainer}>
+              <Typography component = "H3" className={classes.title}>
+                  {title}
+              </Typography>
+              <IconButton className = {classes.arrow} onClick={()=>{this.getBack()}}>
+                <ArrowBackIosIcon color ="secondary" fontSize = "large" color = "primary" classes = {{colorPrimary: 'black' }} style = {{fontSize: 45}}/>
+              </IconButton>
+            </div>
+          ):(
+            <Typography component = "H3" className={classes.title}>
+                CATEGORIES
+            </Typography>
+          )}
+
         </Typography>
-      </Typography>
 
-      <Grid className = {classes.categories} container justify="space-between" spacing = {0}>
+        <Grid className = {classes.categories} container justify="space-around" spacing = {0}>
 
-      {
-      categories.map((categorie)=>{
-        return(
+        {
+        curent.map((categorie,index)=>{
+          return(
 
-            <Grid key = {categorie.id} className = {classes.categorie} item container direction='column'>
-              <Grid item>
-                <Typography component="div" style={{ backgroundColor: 'white', height: '30vh' }} >
-                  <img className = {classes.img} alt = "imagae présentation" src ={categorie.src} />
-                </Typography>
+              <Grid key = {index} className = {classes.categorie} item xs = {3} container direction='column'>
+                <Grid item>
+                  <Typography component="div" style={{ backgroundColor: 'white', maxHeight: '10%'}} >
+                    <img className = {classes.img} alt = "image présentation" src ={categorie.src} onClick = {(e)=>{this.handleClick(index)}}/>
+                  </Typography>
+                </Grid>
+                <Grid item >
+                  <Typography className= {classes.name}  >
+                    {categorie.name}
+                  </Typography>
+                </Grid>
               </Grid>
-              <Grid item >
-                <Typography className= {classes.title}  >
-                  {categorie.name}
-                </Typography>
-              </Grid>
-            </Grid>
 
-        )
-      })
-    }
+          )
+        })
+      }
 
-      </Grid>
-    </div>
-  )
+        </Grid>
+      </div>
+    )
+  }
 }
+export default withStyles(styles)(Categories);
